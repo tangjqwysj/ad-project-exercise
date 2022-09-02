f9d08f1d89999717b71880fa6dc9883b1a0b7678
tangjqwysj <tangjqwysj@sina.com>
2022-09-02 19:58:05 +0800

    init

---
+++ config/webpack.dev.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpackBaseConfig = require('./webpack.base.config')
+
+const webpackDevConfig = {
+   mode: 'development',
+   plugins: [
+      new HtmlWebpackPlugin({
+         title: 'React Exercise',
+         inject: 'body',
+         filename: 'index.html',
+         template: path.join(__dirname, '../src/index.html')
+      })
+   ],
+   devServer: {
+      hot: true,
+      port: 9507,
+      open: true
+   }
+}
+
+module.exports = merge(webpackBaseConfig, webpackDevConfig)
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import './style.scss'
+
+class App extends Component{
+   render(){
+      return (
+         <div className="box">
+            hahah
+            <span>xxxx</span>
+         </div>
+      )
+   }
+}
+
+export default App
